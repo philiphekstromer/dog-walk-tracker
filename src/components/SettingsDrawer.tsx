0d6e9600ac273994c0aaac8010847c8d1317548d
Philiphekstromer <p.ekstromer@gmail.com>
2026-03-04 21:24:18 +0100

    feat: add boilerplate implementation

--- src/components/SettingsDrawer.tsx
+++ src/components/SettingsDrawer.tsx
@@ -1,3 +1,5 @@
+import { BottomSheet, useBottomSheet } from "@plainsheet/react";
+
 type SettingsDrawerProps = {
   intervalHours: number;
   onChangeInterval: (hours: number) => void;
@@ -7,28 +9,43 @@ export const SettingsDrawer = ({
   intervalHours,
   onChangeInterval,
 }: SettingsDrawerProps) => {
+  const bottomSheet = useBottomSheet({
+    rootStyle: { backgroundColor: "transparent" },
+    backdropColor: "rgba(0, 0, 0, 0.4)",
+  });
+
+  const handleButtonPress = () => {
+    bottomSheet.open();
+    //wait for the bottom sheet to open before snapping to the desired position. This ensures that the snapTo function is called after the bottom sheet has been rendered and is ready to be manipulated.
+    setTimeout(() => {
+      bottomSheet.snapTo(0.5); // Snap to 50% of the screen height
+    }, 0);
+  };
+
   return (
-    //This should be a drawer, but for simplicity, it's currently just a card with buttons. Will be replaced with a proper drawer in the future.
-    <div className="card">
-      Set interval between walks:
-      <button
-        style={{
-          padding: "0.25rem 0.5rem",
-          backgroundColor: intervalHours === 4 ? "#1a1a1a" : "#ccc",
-        }}
-        onClick={() => onChangeInterval(4)}
-      >
-        4
-      </button>
-      <button
-        style={{
-          padding: "0.25rem 0.5rem",
-          backgroundColor: intervalHours === 6 ? "#1a1a1a" : "#ccc",
-        }}
-        onClick={() => onChangeInterval(6)}
-      >
-        6
-      </button>
-    </div>
+    <>
+      <BottomSheet {...bottomSheet.props}>
+        Set interval between walks:
+        <button
+          style={{
+            padding: "0.25rem 0.5rem",
+            backgroundColor: intervalHours === 4 ? "#1a1a1a" : "#ccc",
+          }}
+          onClick={() => onChangeInterval(4)}
+        >
+          4
+        </button>
+        <button
+          style={{
+            padding: "0.25rem 0.5rem",
+            backgroundColor: intervalHours === 6 ? "#1a1a1a" : "#ccc",
+          }}
+          onClick={() => onChangeInterval(6)}
+        >
+          6
+        </button>
+      </BottomSheet>
+      <button onClick={handleButtonPress}>Settings</button>
+    </>
   );
 };
