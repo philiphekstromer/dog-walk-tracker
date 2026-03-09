c30b4225928934e210c521b4230d78b8ab4590c2
Philip <143160639+philiphekstromer@users.noreply.github.com>
2026-03-09 20:53:25 +0100

    feat: add release drawer
    add drawer to show release notes when new features are shipped

---
+++ src/components/ReleaseNotesDrawer.tsx
@@ -0,0 +1,53 @@
+import { useAppDrawer } from "../hooks/useAppDrawer";
+import { AppDrawer } from "./AppDrawer";
+import { useEffect } from "react";
+
+type ReleaseNotesDrawerProps = {
+  snapHeightPercent: number;
+  content: React.ReactNode;
+  version: string;
+};
+
+export const ReleaseNotesDrawer = ({
+  snapHeightPercent,
+  content,
+  version,
+}: ReleaseNotesDrawerProps) => {
+  const appDrawer = useAppDrawer();
+
+  useEffect(() => {
+    const lastSeenVersion = localStorage.getItem("lastSeenVersion");
+
+    if (lastSeenVersion !== version) {
+      setTimeout(() => {
+        appDrawer.open();
+        appDrawer.snapTo(snapHeightPercent);
+        localStorage.setItem("lastSeenVersion", version);
+      }, 500);
+    }
+  }, [appDrawer, version, snapHeightPercent]);
+
+  return (
+    <AppDrawer drawer={appDrawer}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "baseline",
+            flexDirection: "row",
+            gap: "1rem",
+          }}
+        >
+          <h2>✨ What's new </h2>
+          <h3>v. {version}</h3>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <div style={{ textAlign: "left", fontSize: "0.95rem" }}>
+            {content}
+          </div>
+          <button onClick={() => appDrawer.close()}>Got it!</button>
+        </div>
+      </div>
+    </AppDrawer>
+  );
+};
