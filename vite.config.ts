7db7a544ead5b086f778d075723137b552d9a537
Philiphekstromer <p.ekstromer@gmail.com>
2026-05-16 08:26:18 +0200

    build: change to dynamic base-url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/dog-walk-tracker/',
+    // DYNAMIC BASE-URL:
+  // if VITE_IS_PREVIEW is set to "true" in our GitHub Action, we use a relative path for pr-preview action.
+  base: process.env.VITE_IS_PREVIEW === 'true' ? '' : '/dog-walk-tracker/',
   plugins: [react()],
 })
